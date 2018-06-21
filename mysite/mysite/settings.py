b06f1784ba0bb2666f68eeefc73734c8ef7819e8
Kang <synod2@naver.com>
2018-06-21 13:31:56 +0000

    0621_setting

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = '@s(qt@o(5lkf!)a$u!b(d!c%ps&qefy5ot43f5wg*%g57ea*&o'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['*']
 
 
 # Application definition
---
+++ va.py
@@ -0,0 +1,12 @@
+import os
+#-*- coding:utf-8 -*-
+os.system("/bin/bash --rcfile ~/workspace/polls_env/bin/activate")
+#if os.system(". ~/workspace/polls_env/bin/activate") == 0 :
+#    print("\n Virturl Env system activated! \n")
+#else :
+#    print("\n Virtual Env start Failed. \n")
+    
+#print("현재 파이썬 버전 : ")
+#os.system("python -V");
+#print("\현재 장고 버전 : ")
+#os.system("python -m django --version")
